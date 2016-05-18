1cc4139508c23eceeaf6d14a3ae13739fb9d28a3
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-18 22:20:03 +0300

    Add more weather info.

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function fetchWeatherData(city) {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
       var weatherData = JSON.parse(xmlhttp.responseText);
       renderData(weatherData);
-      console.log(weatherData);
     }
   };
 
@@ -22,5 +21,12 @@ function fetchWeatherData(city) {
 
 function renderData(weatherData) {
   document.getElementById('name-output').innerHTML = weatherData.name;
-  document.getElementById('temp-output').innerHTML = weatherData.main.temp;
+  document.getElementById('country-name-output').innerHTML = weatherData.sys.country;
+  document.getElementById('temp-output').innerHTML = kelvinToCelcius(weatherData.main.temp);
+  document.getElementById('weather-type-output').innerHTML = weatherData.weather[0].main;
+  console.log(weatherData);
+}
+
+function kelvinToCelcius(kelvin) {
+  return kelvin - 273.15;
 }
