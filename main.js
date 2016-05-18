b5f539f6bdd355bfaa4389c9f9e441b59c2e75f7
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-18 03:29:48 +0300

    Adds loadXml() function, enables loading of an example XML from the api, when submit button is pressed.

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("hello");
+
+
+function loadXml(city) {
+  var xhttp, xmlDoc, txt, x, i;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    xmlDoc = xhttp.responseXML;
+    var xmlText = new XMLSerializer().serializeToString(xmlDoc);
+
+    document.getElementById("demo").innerHTML = xmlText;
+    }
+  };
+  xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&mode=xml&appid=82f22ae917a8c8565e5e1da1bcaef58d", true);
+  xhttp.send();
+}
+
+var form = document.getElementById('text');
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var city = myTextArea.value;
+	loadXml(city);
+});
