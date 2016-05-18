952d193b5342dba9108100ef0fcb4d7757a16537
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-18 21:56:51 +0300

    Add city variable to the weather request. Outputs city name and temperature.

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ var form = document.getElementById('weather-search-form');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
   var city = document.getElementById('city-input').value;
-  fetchWeatherData();
+  fetchWeatherData(city);
 });
 
-function fetchWeatherData() {
+function fetchWeatherData(city) {
   var xmlhttp = new XMLHttpRequest();
 
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
       var weatherData = JSON.parse(xmlhttp.responseText);
       renderData(weatherData);
+      console.log(weatherData);
     }
   };
 
-  xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=82f22ae917a8c8565e5e1da1bcaef58d', true);
+  xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=82f22ae917a8c8565e5e1da1bcaef58d', true);
   xmlhttp.send();
 }
 
 function renderData(weatherData) {
-  document.getElementById('output').innerHTML = weatherData.name;
+  document.getElementById('name-output').innerHTML = weatherData.name;
+  document.getElementById('temp-output').innerHTML = weatherData.main.temp;
 }
