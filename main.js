71dc4bfd7017416d8978405ecd39f40e1b5f7625
Areii1 <ari.jaaskelainen1@gmail.com>
2016-05-18 06:05:58 +0300

    Refactors existing functionality, adds local practice xml file.

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-
-
-function loadXml(city) {
-  var xhttp, xmlDoc, txt, x, i;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    xmlDoc = xhttp.responseXML;
-    var xmlText = new XMLSerializer().serializeToString(xmlDoc);
-
-    document.getElementById("demo").innerHTML = xmlText;
-    }
-  };
-  xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&mode=xml&appid=82f22ae917a8c8565e5e1da1bcaef58d", true);
-  xhttp.send();
-}
-
 var form = document.getElementById('text');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 	var city = myTextArea.value;
-	loadXml(city);
+	loadDoc();
 });
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+    myFunction(xhttp);
+    }
+  };
+  xhttp.open("GET", "weather.xml", true);
+  xhttp.send();
+}
+
+function myFunction(xml) {
+  var xmlDoc = xml.responseXML;
+  var xmlText = (new XMLSerializer()).serializeToString(xmlDoc);
+
+  document.getElementById("demo").innerHTML = xmlText;
+}
